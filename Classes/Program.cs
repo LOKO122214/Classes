a47fcb429b828777d532cb7aff40b121e2ff2563
LOKO122214 <ivailoarabadzhiev0@gmail.com>
2025-09-28 19:56:33 +0300

    First

---
+++ Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using Classes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*// Create a new Point object using constructor
+        Point p1 = new Point(5, 10);
+
+        // Change values using properties
+        p1.X = 20;
+        p1.Y = 30;*/
+        Rectangle area1 = new Rectangle(5, 10);
+        area1.S();
+    }
+}
---
+++ Classes/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Rectangle
+    {
+        // Private fields
+        private int l;
+        private int w;
+
+        // Public properties
+        public int L
+        {
+            get { return l; }
+            set { l = value; }
+        }
+
+        public int W
+        {
+            get { return w; }
+            set { w = value; }
+        }
+
+        // Constructor
+        public Rectangle(int l, int w)
+        {
+            this.l = l;
+            this.w = w;
+        }
+        
+        public void S()
+        {
+            Console.WriteLine($"The area of the rectangle is{l*w}" );
+        }
+    }
+}
